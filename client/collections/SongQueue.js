b91ca5db2a87e7e680402bae264cbb5b1aedd0d9
lensvelt <lensvelt.brett@gmail.com>
2016-05-04 22:00:33 -0700

    Complete Song Queue Spec

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,28 +1,36 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Backbone.Collection.extend({
-
+  
   model: SongModel,
-  queue: [],
-  length: 0,
 
   initialize: function() {
+    
     this.on('ended', function() {
-      this.queue.shift();
-      this.length = this.queue.length;
+      this.shift();
+      if (this.length) {
+        this.playFirst();
+      }
     });
+
+    this.on('add', function() {
+      if (this.models.length === 1) {
+        this.playFirst();
+      } 
+    });
+
+    this.on('dequeue', function() {
+      this.shift();
+      if (this.length) {
+        this.playFirst();
+      }
+    });
+
   },
 
-  add: function(song) {
-    this.queue.push(song);
-    console.log("queue: ", this.queue);
-    if (this.queue.length === 1) {
-      this.playFirst(song);
-    }
-    this.length = this.queue.length;
-  },
+  playFirst: function() {
+    this.models[0].play();
+  }
 
-  playFirst: function(song) {
-    this.queue[0].play;
-  },
+
 
 });
