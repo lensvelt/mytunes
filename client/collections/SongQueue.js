776908bf2ecde5d0a914828d93fe9e0f5eee39de
lensvelt <lensvelt.brett@gmail.com>
2016-05-04 21:14:36 -0700

    Complete Song Spec, started on SongQueue spec

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,8 +2,27 @@
 var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
+  queue: [],
+  length: 0,
 
   initialize: function() {
-  }
+    this.on('ended', function() {
+      this.queue.shift();
+      this.length = this.queue.length;
+    });
+  },
+
+  add: function(song) {
+    this.queue.push(song);
+    console.log("queue: ", this.queue);
+    if (this.queue.length === 1) {
+      this.playFirst(song);
+    }
+    this.length = this.queue.length;
+  },
+
+  playFirst: function(song) {
+    this.queue[0].play;
+  },
 
 });
